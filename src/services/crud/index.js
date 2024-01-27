c4600970cc8a871b1fe7c7b6ed3d9435c59cec0f
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-27 11:30:46 +0300

    on click button

--- src/services/crud/index.js
+++ src/services/crud/index.js
@@ -7,3 +7,7 @@ export const getList = (modele) => {
 export const ajouter = (data, modele) => {
     return axios.post(`${process.env.REACT_APP_API_URL}/api/${modele}`, data);
 }
+
+export const supprimer = (id, modele) => {
+    return axios.delete(`${process.env.REACT_APP_API_URL}/api/${modele}/${id}`);
+}
