2e9c3e2e3790c2ad03ffbe7ff7dc985de9466710
ManoarySarobidy <manoarysarobidy@gmail.com>
2024-01-28 07:35:34 +0300

    Update moteur.jsx

--- src/pages/back-office/moteur/moteur.jsx
+++ src/pages/back-office/moteur/moteur.jsx
@@ -2,8 +2,62 @@ import FormDiv from "../../../components/formulaire/form";
 import Input from "../../../components/formulaire/input-form";
 import Button from "../../../components/button/button";
 import Select from "../../../components/formulaire/select";
+import {useState,useEffect} from 'react';
+import {getList, ajouter} from '../../../services/crud';
 
 const Moteur = () => {
+
+    const[ brands, setBrands ] = useState([]);
+    const[ engineType, setEngineType ] = useState(null);
+    const[ engines, setEngines ] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+
+    const fetchBrands = () => {
+        let marques = getList("marques");
+        marques.then( response => {
+            setBrands( response.data );
+        } )
+    };
+
+    const fetchEngineType = () =>{
+        let engineTypes = getList("types");
+        engineTypes.then( response => {
+            setEngines( response.data );
+        } )
+    };
+
+    const handleEngineChange = (event) => {
+        let engineId = event.target.value;
+        let engine = engines.find( engine => engine.id === engineId );
+        setEngineType(engine);
+    };
+
+    const ajoutMoteur = (event) => {
+        event.preventDefault();
+        let formData = new FormData(event.target);
+
+        let data = {
+            "marque" : formData.get("marque"),
+            "type" :formData.get("type"),
+            "carburant" :formData.get("carburant"),
+            nom : formData.get('nom'),
+            cylindre : formData.get('cylindre'),
+            puissance : formData.get('puissance'),
+            consommation : formData.get('consommation'),
+            capacite : formData.get('capacite'),
+            configuration : formData.get('configuration')
+        };
+
+        ajouter( data, "moteurs" ).then().catch(
+            error => console.log(error)
+        );
+    };
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchBrands();
+        fetchEngineType();
+    },[isLoading]);
 
     const inputs = [
         {
@@ -36,6 +90,12 @@ const Moteur = () => {
             "type" : "text",
             "placeholder" : "Entrer la capacite du moteur"
         },
+        {
+            "nom" : "configuration",
+            "label" : "Configuration du moteur",
+            "type" : "text",
+            "placeholder" : "Entrer la configuration du moteur"
+        },
     ];
 
     return (
@@ -47,17 +107,38 @@ const Moteur = () => {
             display: 'flex',
             justifyContent: 'center'
         }}>
-            <form>    
+            <form onSubmit={(event) => ajoutMoteur(event)}>    
                 <h2 className='formulaire__title'>Création de moteur</h2>
                 <div style={{ marginBottom: '35px' }}>
                     <div className='input__label'>
                         <label>Marque</label>
                     </div>
                     <Select name={'marque'}>
-                        <option value="">Toyota</option>
-                        <option value="">Volsvagen</option>
-                        <option value="">Nissan</option>
-                        <option value="">Tesla</option>
+                       {
+                        brands && brands.map( brand => {
+                            return (
+                                <option value={brand.id}>
+                                    { brand.nom }
+                                </option>
+                            )
+                        } )
+                       }
+                    </Select>
+                </div>
+                <div style={{ marginBottom: '35px' }}>
+                    <div className='input__label'>
+                        <label>Type de Moteur</label>
+                    </div>
+                    <Select name={'type'} onChange={(event) => handleEngineChange(event)}>
+                        {
+                            engines && engines.map( (engine, index) => {
+                                return (
+                                    <option value={engine.id} key={index}>
+                                        { engine.nom }
+                                    </option>
+                                )
+                            })
+                        }
                     </Select>
                 </div>
                 <div style={{ marginBottom: '35px' }}>
@@ -65,8 +146,16 @@ const Moteur = () => {
                         <label>Carburant</label>
                     </div>
                     <Select name={'carburant'}>
-                        <option value="">Essence</option>
-                        <option value="">Diesel</option>
+                        {
+                            engineType && engineType.carburants.map( carburant => {
+                                return (
+                                    <option value = {carburant.id}>
+                                        { carburant.nom }
+                                    </option>
+
+                                    )
+                            } )
+                        }
                     </Select>
                 </div>
                 {inputs.map(input => {
