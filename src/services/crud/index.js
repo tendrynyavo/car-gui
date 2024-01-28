d4aa6340a1bddef50497da48feb53eddfe5a4b64
ManoarySarobidy <manoarysarobidy@gmail.com>
2024-01-28 17:14:03 +0300

    mampiditra token

--- src/services/crud/index.js
+++ src/services/crud/index.js
@@ -1,13 +1,34 @@
 import axios from 'axios';
 
+let headers = {
+    Authorization : `Bearer ${localStorage.getItem('token')}`
+};
+
 export const getList = (modele) => {
-    return axios.get(`${process.env.REACT_APP_API_URL}/api/${modele}`);
+    
+    // alert( localStorage.getItem('token') );
+    return axios.request({
+        headers : headers,
+        method : "GET",
+        url: `${process.env.REACT_APP_API_URL}/api/${modele}`
+    });
 }
 
 export const ajouter = (data, modele) => {
-    return axios.post(`${process.env.REACT_APP_API_URL}/api/${modele}`, data);
+    return axios.request({
+        headers : headers,
+        method: "POST",
+        url: `${process.env.REACT_APP_API_URL}/api/${modele}`,
+        data: data
+    });
+    // return axios.post(, data);
 }
 
 export const supprimer = (id, modele) => {
-    return axios.delete(`${process.env.REACT_APP_API_URL}/api/${modele}/${id}`);
+    return axios.request({
+        headers : headers,
+        method: "DELETE",
+        url: `${process.env.REACT_APP_API_URL}/api/${modele}`
+    });
+    // return axios.delete(`${process.env.REACT_APP_API_URL}/api/${modele}/${id}`);
 }
