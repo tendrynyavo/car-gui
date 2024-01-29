ba693a84dd3e6886f361087e28abc4ee94917a37
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-29 10:06:19 +0300

    Update useToken.js

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -2,14 +2,15 @@ import { useState } from 'react';
 
 export default function useToken() {
     const getToken = () => {
-        const tokenString = localStorage.getItem('token');
-        return tokenString
+        const tokenString = sessionStorage.getItem('token');
+        const userToken = tokenString;
+        return userToken
     };
 
     const [token, setToken] = useState(getToken());
 
     const saveToken = userToken => {
-        localStorage.setItem('token', userToken);
+        sessionStorage.setItem('token', userToken);
         setToken(userToken);
     };
 
